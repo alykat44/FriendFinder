b3926a2c325a890f6ba6facb7cf236c41cf322c9
Annette Yeager <annetteyeager@Annettes-MacBook-Pro.local>
2018-08-09 15:52:23 -0500

    stuff

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FriendFinder
 
 
-#Project Description
+# Project Description
 
 Looking for a new friend? Use Friend Finder - a short survey of questions that once completed will generate a picture of your new best friend!
 
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 // Dependencies
-var bodyParser = require("body-parser");
+
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 // Set up express
 
 var app = express();
+
 PORT = process.env.PORT || 4000;
 
 
 
 // Routes and images/Data Parsing
 
-app.use(express.static("app/public"));
+/* app.use(express.static("app/public")); */
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
